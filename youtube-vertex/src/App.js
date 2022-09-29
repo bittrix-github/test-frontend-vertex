507ecaa52623239001c815964fa502d39deb1703
Pedro Bittencourt <bittrix@hotmail.com>
2022-09-28 21:10:02 -0300

    cria react-app e setup basico

---
+++ youtube-vertex/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+
+const App = () => {
+  
+  return (
+
+    <div className = 'app'>
+
+      <span>hello world</span>
+
+    </div>
+
+  );
+
+}
+
+
+
+export default App;
